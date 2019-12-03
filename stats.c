73b0914e860296311f9e441c4f1c420b7e8c5db0
Eng-Mohanad <engineer.mohanad@outlook.com>
2019-12-03 08:21:41 +0200

    Adding code outline and documentation

--- stats.c
+++ stats.c
@@ -9,3 +9,52 @@
  * 	_ the statistical analysis purposed for the project.
  * --------------------------------------------------------------------------*/
 
+/* Includes */
+#include <stdio.h>
+#include "stats.h"
+
+/* Global Variables */
+unsigned char g_array[SIZE] = {  34, 201, 190, 154,   8, 194,   2,   6,
+				114,  88,  45,  76, 123,  87,  25,  23,
+				200, 122, 150,  90,  92,  87, 177, 244,
+				201,   6,  12,  60,   8,   2,   5,  67,
+				  7,  87, 250, 230,  99,   3, 100,  90};
+
+/* Function main */
+int main(void){
+
+}
+
+/* User_defined Functions */
+float find_median(unsigned char *a_array, unsigned char a_size){
+
+}
+
+float find_mean(unsigned char *a_array, unsigned char a_size){
+
+}
+
+unsigned char find_maximum(unsigned char *a_array, unsigned char a_size){
+
+}
+
+unsigned char find_minimum(unsigned char *a_array, unsigned char a_size){
+
+}
+
+void print_statistics(unsigned char *a_array, unsigned char a_size){
+
+}
+
+void print_array(unsigned char *a_array, unsigned char a_size){
+
+}
+
+void sort_array(unsigned char *a_array, unsigned char a_size){
+
+}
+
+
+
+
+/* End of File */			/* _stats.c_ */
